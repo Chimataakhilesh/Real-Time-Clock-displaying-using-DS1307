afc39c98c76856898f22a4b8a2a06b43244ce554
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-03-14 20:10:16 +0530

    Update RTC.c

--- RTC.c
+++ RTC.c
@@ -1,65 +1,75 @@
-#include <LPC21XX.H>
+#include <LPC21XX.H>   // LPC21xx microcontroller header
 
-typedef unsigned char u8;
-typedef unsigned int  u32;
+typedef unsigned char u8;   // 8-bit unsigned
+typedef unsigned int  u32;  // 32-bit unsigned
 
-#include "lcd_header.h"
-#include "I2C_header.h"
+#include "lcd_header.h"     // LCD functions
+#include "I2C_header.h"     // I2C functions
 
-#define RTC_SLAVE_ADDR 0x68
+#define RTC_SLAVE_ADDR 0x68 // RTC device address
 
-u8 BCD_to_Decimal(u8 bcd);
-u8 rtc_read(u8 reg_addr);
-void LCD_integer(u8 d);
+u8 BCD_to_Decimal(u8 bcd);  // Function to convert BCD to decimal
+u8 rtc_read(u8 reg_addr);   // Function to read RTC register
+void LCD_integer(u8 d);     // Function to display integer on LCD
 
-int main() {
-    u8 sec, min, hour; // To track changes
+int main() 
+{
+    u8 sec, min, hour; // Variables to store time
 
-    I2C_INIT();
-    LCD_INIT();
-		while(1)
-		{
-			sec = BCD_to_Decimal(rtc_read(0x00));
-			min = BCD_to_Decimal(rtc_read(0x01));
-			hour = BCD_to_Decimal(rtc_read(0x02));
-			
-			
-			LCD_COMMAND(0x80);
-			LCD_integer(hour);
-			LCD_DATA(':');
-			LCD_integer(min);
-			LCD_DATA(':');
-			LCD_integer(sec);
-			
-		}
+    I2C_INIT();   // Initialize I2C
+    LCD_INIT();   // Initialize LCD
+
+    while(1)
+    {
+        // Read seconds, minutes and hours from RTC
+        sec  = BCD_to_Decimal(rtc_read(0x00));
+        min  = BCD_to_Decimal(rtc_read(0x01));
+        hour = BCD_to_Decimal(rtc_read(0x02));
+
+        // Display time on LCD
+        LCD_COMMAND(0x80);   // Move cursor to first position
+        LCD_integer(hour);   // Display hour
+        LCD_DATA(':');       // Print :
+        LCD_integer(min);    // Display minute
+        LCD_DATA(':');       // Print :
+        LCD_integer(sec);    // Display second
+    }
 }
+
+// Function to read data from RTC
 u8 rtc_read(u8 reg_addr)
 {
     u8 data;
 
-    I2C_Start();             
-    I2C_Write(RTC_SLAVE_ADDR << 1);      
-    I2C_Write(reg_addr);     
+    I2C_Start();                         // Start I2C communication
+    I2C_Write(RTC_SLAVE_ADDR << 1);      // Send RTC address (write mode)
+    I2C_Write(reg_addr);                 // Send register address
 
-    I2C_Restart();           
-    I2C_Write((RTC_SLAVE_ADDR << 1) | 1); 
+    I2C_Restart();                       // Restart I2C
+    I2C_Write((RTC_SLAVE_ADDR << 1) | 1);// Send RTC address (read mode)
 
-    data = I2C_Nack();       
-    I2C_Stop();
+    data = I2C_Nack();                   // Read data from RTC
+    I2C_Stop();                          // Stop I2C
 
-    return data;
+    return data;                         // Return received data
 }
-void LCD_integer(u8 d) {
 
-    if(d < 10) {
-        LCD_DATA('0');
-        LCD_DATA(d + '0');
+// Function to display number on LCD
+void LCD_integer(u8 d) 
+{
+    if(d < 10) 
+    {
+        LCD_DATA('0');        // Print leading zero
+        LCD_DATA(d + '0');    // Print digit
     } 
-		else {
-        LCD_DATA((d / 10) + '0');
-        LCD_DATA((d % 10) + '0');
+    else 
+    {
+        LCD_DATA((d / 10) + '0'); // Print tens digit
+        LCD_DATA((d % 10) + '0'); // Print ones digit
     }
 }
+
+// Convert BCD value to decimal
 u8 BCD_to_Decimal(u8 bcd)
 {
     return ((bcd / 16) * 10) + (bcd % 16);
